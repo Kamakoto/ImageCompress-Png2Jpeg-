1af85e111132fc8d0898148b530d42912866f984
Kamakoto <1564082745@qq.com>
2022-06-02 09:49:07 +0800

    Add files via upload

---
+++ ImageCompress/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ImageCompress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "process";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(93, 181);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(358, 22);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "删除源文件 Delete Png after Compress";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(386, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "请将软件和所需处理的图片放置同一个文件夹中";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(494, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please move Exe and Png(wanna compress) in same folder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(265, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "质量 quality(0-100)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 335);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.Text = "ImageCompress(Png->Jpeg)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ImageCompress/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageCompress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int quality = 50;
+            if (int.TryParse(textBox1.Text, out quality))
+            {
+                string[] files = Directory.GetFiles(Application.StartupPath, "*.png");
+                MessageBox.Show($"Pngs Summary:{files.Length}");
+                int summary = 0;
+                foreach (var file in files)
+                {
+                    compressImage(file,quality).Save(Application.StartupPath + "/" + Path.GetFileNameWithoutExtension(file) + ".jpg", ImageFormat.Jpeg);
+                    summary++;
+                    if(checkBox1.Checked)
+                    {
+                        File.Delete(file);
+                    }
+                    label1.Text = $"{summary}/{files.Length}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("ERROR QUALITY");
+            }
+
+
+        }
+
+
+        private System.Drawing.Image compressImage(string fileName, int newQuality)
+        {
+            using (System.Drawing.Image image = System.Drawing.Image.FromFile(fileName))
+            using (System.Drawing.Image memImage = new Bitmap(image, image.Width, image.Height)) //如果需要自定义长宽，请把参数提取至构造方法
+            {
+                ImageCodecInfo myImageCodecInfo;
+                System.Drawing.Imaging.Encoder myEncoder;
+                EncoderParameter myEncoderParameter;
+                EncoderParameters myEncoderParameters;
+                myImageCodecInfo = GetEncoderInfo("image/jpeg");
+                myEncoder = System.Drawing.Imaging.Encoder.Quality;
+                myEncoderParameters = new EncoderParameters(1);
+                myEncoderParameter = new EncoderParameter(myEncoder, newQuality);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                MemoryStream memStream = new MemoryStream();
+                memImage.Save(memStream, myImageCodecInfo, myEncoderParameters);
+                System.Drawing.Image newImage = System.Drawing.Image.FromStream(memStream);
+                ImageAttributes imageAttributes = new ImageAttributes();
+                using (Graphics g = Graphics.FromImage(newImage))
+                {
+                    g.InterpolationMode =
+                      System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;  //**
+                    g.DrawImage(newImage, new Rectangle(System.Drawing.Point.Empty, newImage.Size), 0, 0,
+                      newImage.Width, newImage.Height, GraphicsUnit.Pixel, imageAttributes);
+                }
+                return newImage;
+            }
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo ici in encoders)
+                if (ici.MimeType == mimeType) return ici;
+
+            return null;
+        }
+    }
+}
